9b9ece1dde69a61ee1c2e9f0fd33e3d2fe6ed6c3
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2023-11-28 11:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function numberOfDays(year){
 	function isleapyear(year){
-		return ((year%4===0 &&  year%100!==0)||(year%400===0))
+		return ((year%4===0 &&  year%100!==0)||(year%400===0));
 	}
-	return isleapyear(year) ? 366 : 365;
+	return (isleapyear(year) ? 366 : 365);
 }
 let year=prompt("enter year");
