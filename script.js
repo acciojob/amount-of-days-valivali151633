04b7551075a74c211400f018cbeb90b5318dd17c
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2023-11-28 11:20:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let years = prompt(" enter year");
+function numberOFDays(year){
+	if(year % 4 === 0)
+{
+	if(year%100=== 0)
+	{
+		if(year%400 ===0)
+		{
+			console.log(366)
+		}
+		else
+		{
+			coonsole.log(365)
+		}
+	}
+	else
+	{
+		console.log(366)
+	}
+}
+else
+{
+	console.log(365)
+}
+}
+numberOFDays(years);
